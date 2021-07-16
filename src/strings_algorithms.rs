ea1d06ab704e48413748373f958ae9c1e9390a51
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2021-07-16 06:18:20 -0400

    Added naive pattern search to strings algoritms

--- src/strings_algorithms.rs
+++ src/strings_algorithms.rs
@@ -58,6 +58,22 @@ pub fn is_palindrome(text: &str) -> bool {
             .rev())
 }
 
+pub fn naive_pattern_search(s: String, pattern: &str) -> Vec<i32> {
+    s.char_indices()
+        .map(|(i, x)| {
+            let myv = pattern.chars().nth(0).unwrap();
+
+            if x == myv {
+                (i as i32, &s[i..(i + (pattern.len() - 1))])
+            } else {
+                (i as i32, "")
+            }
+        })
+        .filter(|(_i, x)| pattern == *x)
+        .map(|(i, _x)| i)
+        .collect()
+}
+
 /// Remove duplicates from sentence
 pub fn remove_duplicates(text: String) -> String {
     let modified_text_vec = text.split_whitespace().collect::<Vec<&str>>();
