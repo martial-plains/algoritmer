1ad7ce6d193181258039bb4eb2232d2f2fc5aa54
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-04 05:48:45 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+//! # Algorithms for Rust
+
+#![feature(decl_macro)]
+#![warn(clippy::all, missing_debug_implementations)]
+
 pub mod dynamic_programming;
+pub mod macros;
 pub mod math;
 pub mod sorts;
 pub mod strings;
