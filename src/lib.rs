a06fb5cbb755b133ec26cb4ee7cb05c42d91e48e
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-04 05:19:49 -0400

    Remove unnecessary test function

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,4 @@
 pub mod dynamic_programming;
 pub mod math;
-pub mod strings;
-
 pub mod sorts;
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod strings;
