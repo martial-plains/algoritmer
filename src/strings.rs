d6394dbeb2e77498dbe02fb1f1e615313373030a
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-22 09:45:32 -0400

    Add brute_force algorithm

--- src/strings.rs
+++ src/strings.rs
@@ -1,4 +1,31 @@
-use std::{collections::HashMap, str};
+use std::{collections::HashMap, mem, str};
+
+/// Find a pattern in a string by comparing the pattern to every substring
+///
+/// # Arguments
+///
+/// * `text` - Any string that might contain the pattern.
+/// * `pattern` - String that we are searching for.
+///
+/// # Returns
+///
+/// Index where the pattern starts in the text
+/// `None` if the pattern is not found
+pub fn brute_force(text: &str, pattern: &str) -> Option<usize> {
+    let pat_l = pattern.len();
+    let txt_l = text.len();
+    let mut i = 0;
+    if pat_l > txt_l {
+        return None;
+    }
+    while i <= txt_l - pat_l {
+        if &text[i..i + pat_l] == pattern {
+            return Some(i);
+        }
+        i += 1;
+    }
+    None
+}
 
 /// This function will capitalize the first letter of a sentence or a word
 pub fn capitalize(sentence: &String) -> String {
@@ -111,9 +138,7 @@ pub fn word_occurrences(text: &str) -> HashMap<&str, u32> {
 
 #[cfg(test)]
 mod tests {
-    extern crate test_case;
-
-    use algorithms::strings::*;
+    use super::*;
     use std::collections::HashMap;
     use test_case::test_case;
 
@@ -174,5 +199,28 @@ mod tests {
         expected.insert("World", 1);
         let actual = word_occurrences("Hello World");
         assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn brute_force_which_works() {
+        let test_set = [
+            ["a", "aa", "-1"],
+            ["a", "a", "0"],
+            ["ba", "b", "0"],
+            ["bba", "bb", "0"],
+            ["bbca", "c", "2"],
+            ["ab", "b", "1"],
+        ];
+
+        for test in test_set.iter() {
+            let actual = brute_force(test[0], test[1]);
+            assert_eq!(
+                test[2].parse::<isize>().unwrap(),
+                match actual {
+                    Some(val) => val as isize,
+                    None => -1,
+                }
+            );
+        }
     }
 }
