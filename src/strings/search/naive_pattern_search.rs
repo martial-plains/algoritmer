7a1e480bae413a09986f47a564ef6be00056069a
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-23 10:46:13 -0400

    Add tests

--- src/strings/search/naive_pattern_search.rs
+++ src/strings/search/naive_pattern_search.rs
@@ -2,18 +2,65 @@ use alloc::vec::Vec;
 
 /// A simple and inefficient way to see where one string occurs inside another
 /// is to check each place it could be, one by one, to see if it's there.
-pub fn naive_pattern_search<'a>(s: &'a str, pattern: &'a str) -> Result<Vec<i32>, &'a str> {
-    Ok(s.char_indices()
-        .map(|(i, x)| {
-            let myv = pattern.chars().nth(0).unwrap();
+///
+/// This algorithm tries to find the pattern from every position of
+/// the main string if pattern is found from position i it adds it to
+/// the answer and does the same for position i+1
+///
+/// Complexity : O(n*m)
+///     n=length of main string
+///     m=length of pattern string
+///
+/// # Examples
+///
+/// ```rust
+/// use algorithms::strings::search::naive_pattern_search;
+///
+/// fn main() {
+///     println!("{:?}", naive_pattern_search("ABAAABCDBBABCDDEBCABC", "ABC"));
+///     println!("{:?}", naive_pattern_search("ABC", "ABAAABCDBBABCDDEBCABC"));
+///     println!("{:?}", naive_pattern_search("", "ABC"));
+///     println!("{:?}", naive_pattern_search("TEST", "TEST"));
+///     println!("{:?}", naive_pattern_search("ABCDEGFTEST", "TEST"));
+/// }
+/// ```
+///
+/// # References
+///
+/// wiki: <https://en.wikipedia.org/wiki/String-searching_algorithm#Na%C3%AFve_string_search>
+pub fn naive_pattern_search<'a>(s: &'a str, pattern: &'a str) -> Vec<usize> {
+    let pat_len = pattern.len();
+    let mut positions = Vec::new();
 
-            if x == myv {
-                (i as i32, &s[i..(i + (pattern.len() - 1))])
-            } else {
-                (i as i32, "")
+    if pat_len > s.len() {
+        return alloc::vec![];
+    }
+
+    for i in 0..s.len() - pat_len + 1 {
+        let mut match_found = true;
+        for j in 0..pat_len {
+            if s.chars().nth(i + j).unwrap() != pattern.chars().nth(j).unwrap() {
+                match_found = false;
+                break;
             }
-        })
-        .filter(|(_i, x)| pattern == *x)
-        .map(|(i, _x)| i)
-        .collect::<Vec<i32>>())
+        }
+        if match_found {
+            positions.push(i)
+        }
+    }
+
+    positions
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use test_case::test_case;
+
+    #[test_case(("ABCDEFG", "DE"))]
+    fn test_naive_pattern_search(data: (&str, &str)) {
+        let actual = naive_pattern_search(data.0, data.1);
+        assert_eq!(actual, [3]);
+    }
 }
