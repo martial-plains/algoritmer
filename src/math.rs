4de953644ce3cc9ee07d1974a8da9680672f2029
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-24 06:57:31 -0400

    Update math.rs

--- src/math.rs
+++ src/math.rs
@@ -1,17 +1,9 @@
 //! Perform mathematical operations and manipulate integer, float, and double values.
 
-mod abs;
 mod calculate_factorial;
-mod ceil;
-mod floor;
 mod is_perfect;
-mod power;
 
 pub mod fibonacci;
 
-pub use abs::*;
 pub use calculate_factorial::calculate_factorial;
-pub use ceil::ceil;
-pub use floor::floor;
 pub use is_perfect::is_perfect;
-pub use power::power;
