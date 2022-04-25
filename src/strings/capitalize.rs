1b9a1ff5b9f6ba27fbe2b07ffcec0b3f549b94b5
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-25 10:16:41 -0400

    Update capitalize.rs

--- src/strings/capitalize.rs
+++ src/strings/capitalize.rs
@@ -5,7 +5,7 @@ pub fn capitalize(sentence: &str) -> String {
     if *sentence != String::new() {
         let mut new_string = String::new();
         for (i, ch) in sentence.char_indices() {
-            if i == 0 && sentence.starts_with(|c| c >= 'a' && c <= 'z') {
+            if i == 0 && sentence.starts_with(|c| ('a'..='z').contains(&c)) {
                 new_string.insert(i, ch.to_ascii_uppercase());
             } else {
                 new_string.insert(i, ch);
@@ -30,7 +30,7 @@ mod tests {
     #[test_case("a", "A")]
     #[test_case("", "")]
     fn does_it_capitalize(sentence: &str, expected: &str) {
-        let actual = capitalize(&mut String::from(sentence));
+        let actual = capitalize(&String::from(sentence));
         assert_eq!(expected, actual);
     }
 }
