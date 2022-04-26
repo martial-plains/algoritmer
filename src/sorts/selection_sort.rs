ccb500f5cd9e4cabef0b9870401918d223eae052
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-26 12:53:13 -0400

    Add selection sort

---
+++ src/sorts/selection_sort.rs
@@ -0,0 +1,49 @@
+/// The selection sort algorithm selects the smallest element from an 
+/// unsorted array in each iteration and places it at the beginning of 
+/// the array.
+/// 
+/// # Arguments
+/// 
+/// * `array` - The array to sort.
+/// 
+/// # Performance
+/// 
+/// ## Time Complexity
+/// 
+/// - Worst: O(n²)
+///     - The worst case occurs when the array is in reverse order.
+/// - Best: O(n²)
+///     - The best case occurs when the array is already sorted.
+/// - Average: O(n²)
+///     - The average case occurs when the array is randomly ordered.
+/// 
+/// ## Space Complexity
+/// 
+/// - O(1)
+/// 
+/// ## Stable
+/// 
+/// No
+/// 
+/// # See Also
+/// 
+/// wiki: <https://en.wikipedia.org/wiki/Selection_sort>
+pub fn selection_sort<T>(array: &mut [T])
+where
+    T: Ord,
+{
+    for i in 0..array.len() {
+        let mut min_idx = i;
+        // For `j` from `i` to the end of the array, find the smallest 
+        // element in the array. Where `j` is the index of the first 
+        // unsorted element.
+        for j in i..array.len() {
+            // Compare the current element with the min element.
+            if array[j] < array[min_idx] {
+                min_idx = j;
+            }
+        }
+        // Swap the current element with the min element.
+        array.swap(i, min_idx);
+    }
+}
