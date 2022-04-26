21eb969a5420576bb25f8b2711ba305442e79a2e
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-26 13:22:27 -0400

    Update merge sort

--- src/sorts/merge_sort.rs
+++ src/sorts/merge_sort.rs
@@ -24,21 +24,21 @@ fn merge<T: Ord + Copy>(arr: &mut [T], mid: usize) {
 /// # Arguments
 ///
 /// * `arr` - The array to sort.
-/// 
+///
 /// # Performance
-/// 
+///
 /// ## Time Complexity
-/// 
+///
 /// - Best: O(n*log n)
 /// - Worst: O(n*log n)
 /// - Average: O(n*log n)
-/// 
+///
 /// ## Space Complexity
-/// 
+///
 /// O(n)
-/// 
+///
 /// # Stability
-/// 
+///
 /// Stable
 pub fn merge_sort<T>(arr: &mut [T])
 where
@@ -52,5 +52,23 @@ where
         merge_sort(&mut arr[mid..]);
         // Combine the two halves.
         merge(arr, mid);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::vec;
+    use alloc::vec::Vec;
+
+    use test_case::test_case;
+
+    use super::*;
+
+    #[test_case( vec![25, 26, 22, 24, 27, 23, 21],  vec![21, 22, 23, 24, 25, 26, 27])]
+    #[test_case( vec![26, 17, 20, 11, 23, 21, 13, 18, 24, 14, 12, 22, 16, 16, 15, 19, 25],  vec![11, 12, 13, 14, 15, 16, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26])]
+    fn test_shell_sort(mut arr: Vec<isize>, expected: Vec<isize>) {
+        merge_sort(&mut arr);
+        let actual = arr;
+        assert_eq!(actual, expected);
     }
 }
