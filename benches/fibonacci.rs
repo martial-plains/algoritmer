55c3db4701a90f4f02b2931ad7f18980de0f7f31
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-27 08:43:20 -0400

    add analytic function to fibonacci

---
+++ benches/fibonacci.rs
@@ -0,0 +1,30 @@
+#![feature(test)]
+
+extern crate test;
+
+use algorithms::math::fibonacci::*;
+
+use test::Bencher;
+
+// bench: find the `BENCH_SIZE` first terms of the fibonacci sequence
+static BENCH_SIZE: usize = 1000;
+
+#[bench]
+fn recursive_fibonacci(b: &mut Bencher) {
+    b.iter(|| (0..BENCH_SIZE).map(recursive).collect::<Vec<_>>());
+}
+
+#[bench]
+fn iterative_fibonacci(b: &mut Bencher) {
+    b.iter(|| (0..BENCH_SIZE).map(iterative).collect::<Vec<_>>())
+}
+
+#[bench]
+fn memoized_fibonacci(b: &mut Bencher) {
+    b.iter(|| (0..BENCH_SIZE).map(memoized).collect::<Vec<_>>())
+}
+
+#[bench]
+fn analytic_fibonacci(b: &mut Bencher) {
+    b.iter(|| (0..BENCH_SIZE).map(analytic).collect::<Vec<_>>())
+}
--- src/math/fibonacci.rs
+++ src/math/fibonacci.rs
@@ -16,16 +16,22 @@ pub fn recursive(nth: usize) -> u128 {
 }
 
 /// Calculate the nth Fibonacci number imperatively.
-pub fn imperative(nth: usize) -> u128 {
-    let mut a = 0;
-    let mut b = 1;
-    let mut c = 0;
-    for _ in 1..nth {
-        c = a + b;
-        a = b;
-        b = c;
+pub fn iterative(nth: usize) -> u128 {
+    match nth {
+        0 => 0,
+        1 => 1,
+        2 => 1,
+        _ => {
+            let mut prev = 1;
+            let mut curr = 1;
+            for _ in 2..nth {
+                let temp = curr;
+                curr += prev;
+                prev = temp;
+            }
+            curr
+        }
     }
-    c
 }
 
 /// Calculate the nth Fibonacci number using dynamic_programming and memoization.
@@ -38,4 +44,51 @@ pub fn memoized(nth: usize) -> u128 {
         }
     }
     memoize(&mut HashMap::new(), fib_memo, nth)
+}
+
+/// Calculate the nth Fibonacci number using Binet's formula.
+pub fn analytic(nth: usize) -> u128 {
+    let sqrt_5 = 5f64.sqrt();
+    let phi = (1. + sqrt_5) / 2.;
+    let q = 1. / phi;
+    ((phi.powf(nth as f64) + q.powf(nth as f64)) / sqrt_5 + 0.5) as u128
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_recursive() {
+        assert_eq!(super::recursive(1), 1);
+        assert_eq!(super::recursive(2), 1);
+        assert_eq!(super::recursive(3), 2);
+        assert_eq!(super::recursive(4), 3);
+        assert_eq!(super::recursive(5), 5);
+    }
+
+    #[test]
+    fn test_iterative() {
+        assert_eq!(super::iterative(1), 1);
+        assert_eq!(super::iterative(2), 1);
+        assert_eq!(super::iterative(3), 2);
+        assert_eq!(super::iterative(4), 3);
+        assert_eq!(super::iterative(5), 5);
+    }
+
+    #[test]
+    fn test_memoized() {
+        assert_eq!(super::memoized(1), 1);
+        assert_eq!(super::memoized(2), 1);
+        assert_eq!(super::memoized(3), 2);
+        assert_eq!(super::memoized(4), 3);
+        assert_eq!(super::memoized(5), 5);
+    }
+
+    #[test]
+    fn test_analytic() {
+        assert_eq!(super::analytic(1), 1);
+        assert_eq!(super::analytic(2), 1);
+        assert_eq!(super::analytic(3), 2);
+        assert_eq!(super::analytic(4), 3);
+        assert_eq!(super::analytic(5), 5);
+    }
 }
