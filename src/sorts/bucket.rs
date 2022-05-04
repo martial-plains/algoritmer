24bda1dc199de6a47962cd55ba208d777352f9a9
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-05-04 08:58:26 -0400

    Update bucket.rs

--- src/sorts/bucket.rs
+++ src/sorts/bucket.rs
@@ -1,31 +1,7 @@
 use alloc::vec::Vec;
 
-/// Illustrate how to implement bucket sort algorithm.
-///
-/// Author: OMKAR PATHAK
-/// This program will illustrate how to implement bucket sort algorithm
-///
-/// Wikipedia says: Bucket sort, or bin sort, is a sorting algorithm that works
-/// by distributing the elements of an array into a number of buckets.
-/// Each bucket is then sorted individually, either using a different sorting
-/// algorithm, or by recursively applying the bucket sorting algorithm. It is a
-/// distribution sort, and is a cousin of radix sort in the most to least
-/// significant digit flavour.
-/// Bucket sort is a generalization of pigeonhole sort. Bucket sort can be
-/// implemented with comparisons and therefore can also be considered a
-/// comparison sort algorithm. The computational complexity estimates involve the
-/// number of buckets.
-///
-/// Time Complexity of Solution:
-/// Worst case scenario occurs when all the elements are placed in a single bucket.
-/// The overall performance would then be dominated by the algorithm used to sort each
-/// bucket. In this case, O(n log n), because of TimSort
-///
-/// Average Case O(n + (n^2)/k + k), where k is the number of buckets
-///
-/// If k = O(n), time complexity is O(n)
-///
-/// Source: <https://en.wikipedia.org/wiki/Bucket_sort>
+/// A Rust bucket sort implementation
+/// Wikipedia: <https://en.wikipedia.org/wiki/Bucket_sort>
 pub fn bucket<T>(arr: &mut [T])
 where
     T: Ord + Clone,
