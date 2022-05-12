5f0c2a8705dcfee3ba2daa608c242a8ccf0bb0c4
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-05-12 12:46:54 -0400

    Update counting.rs

--- src/sorts/counting.rs
+++ src/sorts/counting.rs
@@ -2,18 +2,17 @@ use alloc::vec::Vec;
 
 use core::{cmp::Ord, ops::AddAssign};
 
-
-/// A counting sort implementation for all usigned types
+/// A counting sort implementation for all unsigned types
 pub fn counting<T>(arr: &mut [T])
 where
     T: Ord + Into<f64> + From<u8> + AddAssign + Copy,
 {
     let max = arr.len();
 
-    let mut occurences: Vec<usize> = alloc::vec![0; max + 1];
+    let mut occurrences: Vec<usize> = alloc::vec![0; max + 1];
 
     for &data in arr.iter() {
-        occurences[data.into() as usize] += 1;
+        occurrences[data.into() as usize] += 1;
     }
 
     // Current index in output array
@@ -23,8 +22,8 @@ where
     let mut data = T::from(0);
 
     // This will iterate from 0 to the largest data point in `arr`
-    // `number` contains the occurances of the data point `data`
-    for &number in occurences.iter() {
+    // `number` contains the occurrences of the data point `data`
+    for &number in occurrences.iter() {
         for _ in 0..number {
             arr[i] = data;
             i += 1;
@@ -38,11 +37,11 @@ where
 mod tests {
 
     use super::*;
-   
+
     #[test]
     fn test_counting_sort() {
         let mut arr = [1, 5, 2, 3, 4, 6, 7, 8, 9, 10];
         counting(&mut arr);
         assert_eq!(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    } 
+    }
 }
